f56b2c1454d2da2b0d3ef578f56e5db2953d834b
sourabh kumar <lordgk02@gmail.com>
2026-07-15 15:31:52 +0530

    enabled cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 
